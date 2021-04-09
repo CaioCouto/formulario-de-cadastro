b6f91a63284867b5cc922dbe68d26e2c19de48b7
Caio Couto <coutocaio3@gmail.com>
2021-04-09 11:16:22 -0300

    Criando componente Formulário

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
+import './App.css'
 
 function App() {
   return (
-    <h1>Formulário de Cadastro</h1> 
-  );
+    <>
+      <h1>Formulário de Cadastro</h1> 
+      <FormularioCadastro />
+    </>
+  )
 }
 
-export default App;
+export default App
